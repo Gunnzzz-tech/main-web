41a18defdbb5791816cec66f89789a9b49db624b
gungunbali <gungunbali.hky@gmail.com>
2025-10-21 15:18:07 +0530

    first commit

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ with app.app_context():
 
 # --- Database Model ---
 class Applicant(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     email = db.Column(db.String(150))
